b0e3a21a9262c249c608b42f466bde4685b433a4
naotaka nakane <n.nakane0219@gmail.com>
2021-12-23 08:00:20 +0900

    Install peco.

--- etc/install.sh
+++ etc/install.sh
@@ -13,7 +13,20 @@ install_tmux() {
   ./configure && make && sudo make install
 }
 
+install_peco() {
+  mkdir -p .tmp
+  cd .tmp
+  wget https://github.com/peco/peco/releases/download/v0.5.10/peco_linux_amd64.tar.gz
+  tar xzvf peco_linux_amd64.tar.gz
+  cd peco_linux_amd64
+  sudo cp peco /usr/local/bin
+}
+
 if ! is_exists "tmux"; then
   install_tmux
+fi
+
+if ! is_exists "peco"; then
+  install_peco
 fi
 
