344430273875ad41b38a56ebf181403054aba485
naotaka nakane <n.nakane0219@gmail.com>
2021-12-23 15:10:54 +0900

    Add peco config.

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ ln -sfn "${PWD}/.gitconfig" "${HOME}/.gitconfig"
 ln -sfn "${PWD}/.zshenv" "${HOME}/.zshenv"
 ln -sfn "${PWD}/.vimrc" "${HOME}/.vimrc"
 ln -sfn "${PWD}/.tmux.conf" "${HOME}/.tmux.conf"
+ln -sfn "${PWD}/.peco" "${HOME}/.peco"
 
