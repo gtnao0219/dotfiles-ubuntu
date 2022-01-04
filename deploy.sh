151ad25e47454d29ceb52bf1feeea9f24eae0bc7
naotaka nakane <n.nakane0219@gmail.com>
2022-01-04 14:45:22 +0900

    Add sym link to .zshrc

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 ln -sfn "${PWD}/.gitignore_global" "${HOME}/.gitignore_global"
 ln -sfn "${PWD}/.gitconfig" "${HOME}/.gitconfig"
 ln -sfn "${PWD}/.zshenv" "${HOME}/.zshenv"
+ln -sfn "${PWD}/.zshrc" "${HOME}/.zshrc"
 ln -sfn "${PWD}/.vimrc" "${HOME}/.vimrc"
 ln -sfn "${PWD}/.tmux.conf" "${HOME}/.tmux.conf"
 ln -sfn "${PWD}/.peco" "${HOME}/.peco"
