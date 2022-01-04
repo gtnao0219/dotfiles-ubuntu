fbdc8c0a48e9049519ea3c96af234676c992f543
naotaka nakane <n.nakane0219@gmail.com>
2022-01-04 15:10:24 +0900

    Add download script.

--- etc/install.sh
+++ etc/install.sh
@@ -1,32 +1,46 @@
-#!/bin/sh
-set -eu
+#!/bin/bash
 
-is_exists() {
-  which "$1" > /dev/null 2>&1
-  return $?
-}
+dotfiles_logo='
+@gtnao''s
+   ██████╗  ██████╗ ████████╗███████╗██╗██╗     ███████╗███████╗
+   ██╔══██╗██╔═══██╗╚══██╔══╝██╔════╝██║██║     ██╔════╝██╔════╝
+   ██║  ██║██║   ██║   ██║   █████╗  ██║██║     █████╗  ███████╗
+   ██║  ██║██║   ██║   ██║   ██╔══╝  ██║██║     ██╔══╝  ╚════██║
+██╗██████╔╝╚██████╔╝   ██║   ██║     ██║███████╗███████╗███████║
+╚═╝╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝
+'
 
-install_tmux() {
-  ghq get https://github.com/tmux/tmux.git
-  cd ~/dev/src/github.com/tmux/tmux
-  sh autogen.sh
-  ./configure && make && sudo make install
-}
-
-install_peco() {
-  mkdir -p .tmp
-  cd .tmp
-  wget https://github.com/peco/peco/releases/download/v0.5.10/peco_linux_amd64.tar.gz
-  tar xzvf peco_linux_amd64.tar.gz
-  cd peco_linux_amd64
-  sudo cp peco /usr/local/bin
-}
-
-if ! is_exists "tmux"; then
-  install_tmux
+if [ -z "${DOTPATH:-}" ]; then
+  DOTPATH=~/.dotfiles
+  export DOTPATH
 fi
+DOTFILES_GITHUB="https://github.com/gtnao0219/dotfiles-ubuntu.git"
+export DOTFILES_GITHUB
 
-if ! is_exists "peco"; then
-  install_peco
-fi
+download_dotfiles() {
+  if [ -d "${DOTPATH}" ]; then
+    echo "${DOTPATH}: already exists\n"
+    exit 1
+  fi
+  echo "\n"
+  echo "Downloading dotfiles...\n"
+  local tarball="https://github.com/gtnao0219/dotfiles-ubuntu/archive/refs/heads/master.zip"
+  if is_exists "curl"; then
+    curl -L "${tarball}"
+  elif is_exists "wget"; then
+    wget -O - "${tarball}"
+  else
+    echo "curl or wget required\n"
+    exit 1
+  fi | tar xvz
+  if [ ! -d dotfiles-ubuntu-master ]; then
+    echo "dotfiles-ubuntu-master: not found\n"
+    exit 1
+  fi
+  mv -f dotfiles-ubuntu-master "$DOTPATH"
+  echo "Download\n"
+}
+
+echo "$dotfiles_logo"
+download_dotfiles
 
